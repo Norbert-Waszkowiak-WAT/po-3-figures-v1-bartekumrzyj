02cc6db4166114c6895daa1ee00c1c4e16a58a8c
bartekumrzyj <167216472+bartekumrzyj@users.noreply.github.com>
2024-12-05 07:12:50 +0000

    cos nie pyklo

---
+++ quadrilateral.h
@@ -0,0 +1,22 @@
+#ifndef QUADRILATERAL 
+#define QUADRILATERAL 
+#include "point.h" 
+#include "triangle.h" 
+#include <iostream> 
+class Quadrilateral { 
+    private:
+    Point a; 
+    Point b;    
+    Point c;    
+    Point d; 
+    public:    
+    Quadrilateral();    
+    Quadrilateral(Point a, Point b, Point c, Point d);   
+     Quadrilateral(const Quadrilateral &other);    
+    bool equals(const Quadrilateral &other);    
+    void flip();    
+    void move(double x, double y);    
+    double getSurface();        
+    std::string toString(); 
+    };
+     #endif
